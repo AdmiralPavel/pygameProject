7ae89431504cfc2e5226c0766ed90aa700b3f37a
AdmiralPavel <admiralpavel99@gmail.com>
2021-03-13 16:02:52 +0300

    running alien

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+clock = pygame.time.Clock()
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+player_img = pygame.image.load(os.path.join(img_folder, 'p1_front.png'))
+player_img_hurt = pygame.image.load(os.path.join(img_folder, 'p1_hurt.png'))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Player(pygame.sprite.Sprite):
+    flag = True
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+            if self.flag:
+                self.image = player_img_hurt
+                self.flag = not self.flag
+            else:
+                self.image = player_img
+                self.flag = not self.flag
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption('My game')
+running = True
+
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    all_sprites.update()
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
